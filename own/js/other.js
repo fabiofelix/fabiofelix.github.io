75ac59d41f7421c49e365fb920a52f406b48d2c8
Fábio <f_diasfabio@yahoo.com.br>
2022-07-02 18:32:29 -0300

    Adjust directory structure

--- own/js/other.js
+++ own/js/other.js
@@ -1,9 +1,14 @@
 (function($) 
 {
   if(navigator.language.match("pt") != null && window.location.pathname.match("pt") == null )
-    window.location.replace(window.location + "pt-BR")
+    window.location.replace(window.location + "pt-br")
   else if(navigator.language.match("pt") == null && window.location.pathname.match("pt") != null )    
     window.location.replace(window.location.href.split("pt")[0])
+
+  if (navigator.language.match("pt") != null)
+  {
+    $('.image.thumb').toggleClass('pt-br');    
+  }    
 
   $(".more-less-button").click(function(evnt)
   {
