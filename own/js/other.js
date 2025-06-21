f99e30d2f643f5038bd9c25bb2d2d9bbf86ba832
Fábio <f_diasfabio@yahoo.com.br>
2025-06-21 18:43:51 -0400

    Manage buttons

--- own/js/other.js
+++ own/js/other.js
@@ -1,13 +1,18 @@
-(function($) 
+document.addEventListener("DOMContentLoaded", function()
 {
   if(navigator.language.match("pt") != null && window.location.pathname.match("pt") == null )
     window.location.replace(window.location + "pt-br")
   // else if(navigator.language.match("pt") == null && window.location.pathname.match("pt") != null )    
   //   window.location.replace(window.location.href.split("pt")[0])
 
-  if (navigator.language.match("pt") != null)
-  {
-    $('.image.thumb').toggleClass('pt-br');    
-  }    
+  let btn_desc_more = document.getElementById("desc_more");
 
-})(jQuery);
+  btn_desc_more.addEventListener("click", function(event, target)
+  { 
+    const pt_br = window.location.href.search("pt-br") > -1;
+    const more  = pt_br ? "Mais" : "More";
+    const less  = pt_br ? "Menos" : "Less";
+
+    event.target.innerHTML = event.target.innerHTML === more ? less : more;
+  });
+});
